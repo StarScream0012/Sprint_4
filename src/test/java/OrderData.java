adff5774b69b5a7158b478f5497b33a7c5c1d209
Mansur Akhmetzhan <ahmetzan.m@gmail.com>
2024-08-27 20:31:04 +0500

    commit

---
+++ src/test/java/OrderData.java
@@ -0,0 +1,13 @@
+public class OrderData {
+    String firstName;
+    String secondName;
+    String address;
+    String metroStation;
+    String phone;
+    String date;
+    String color;
+    String comment;
+    String time;
+    int entry;
+
+}
